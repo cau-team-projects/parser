4d84b376ffb3ca592fcc01508548945b171d5eb2
Byeonggon Lee <gonny952@gmail.com>
2020-06-26 06:18:51 +0900

    Change tokens, update parser table

--- parser/table.py
+++ parser/table.py
@@ -37,6 +37,41 @@ table = {
         'semi': ('SHIFT', 9),
         'assign': ('SHIFT', 11),
         'lparen': ('SHIFT', 10),
-    }
+    },
+    8: {
+        'semi': ('SHIFT', 12),
+    },
+    9: {
+        'vtype': ('REDUCE', 4),
+        'id': ('REDUCE', 4),
+        'rbrace', ('REDUCE', 4),
+        'if': ('REDUCE', 4),
+        'while': ('REDUCE', 4),
+        'for': ('REDUCE', 4),
+        'return': ('REDUCE', 4),
+        None: ('REDUCE', 4),
+    },
+    10: {
+        'vtype': ('SHIFT', 14),
+        'rparen': ('REDUCE', 9),
+        'ARG': ('GOTO', 13)
+    },
+    11: {
+        'id': ('SHIFT', 21),
+        'lparen': ('SHIFT', 20),
+        'literal': ('SHIFT', 17),
+        'num': ('SHIFT', 22),
+        'float': ('SHIFT', 23),
+        'RHS': ('GOTO', 15),
+        'EXPR': ('GOTO', 16),
+        'TERM': ('GOTO', 18),
+        'FACTOR': ('GOTO', 19),
+    },
+    12: {
+        'vtype': ('REDUCE', 5),
+        'id': ('REDUCE', 5),
+        'rbrace': ('REDUCE', 5),
+        'if': ('REDUCE', 5),
 
+    }
 }
